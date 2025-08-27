9a7ec09e8f3d9ec06786e390922d535462996383
AbiSulfur <mautidur34@gmail.com>
2025-08-27 11:23:22 +0700

    Delete guru.php

--- guru.php
+++
@@ -1,7 +0,0 @@
-<?php include '../koneksi.php'; ?>
-<?php include '../includes/header.php'; ?>
-
-<h1 class="h3 mb-4">Data Guru</h1>
-<p>(Modul CRUD guru akan ditambahkan di modul berikutnya.)</p>
-
-<?php include '../includes/footer.php'; ?>
