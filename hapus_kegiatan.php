b4230cc607d39aca23c535ffca4f5c20a0162d7f
AbiSulfur <mautidur34@gmail.com>
2025-08-27 11:23:31 +0700

    Delete hapus_kegiatan.php

--- hapus_kegiatan.php
+++
@@ -1,8 +0,0 @@
-<?php include '../koneksi.php'; ?>
-<?php
-$id = intval($_GET['id'] ?? 0);
-if ($id) {
-  mysqli_query($conn, "DELETE FROM kegiatan_guru WHERE id_kegiatan=$id");
-}
-header('Location: kegiatan.php');
-exit;
